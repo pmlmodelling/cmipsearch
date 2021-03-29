1bffe327ad44f30744a4bd558b94dc8abec96b30
Robert Wilson <robertwilson190@gmail.com>
2021-03-29 14:37:36 +0100

    drop dup line causing error

--- cmipsearch/cmip6.py
+++ cmipsearch/cmip6.py
@@ -1,5 +1,3 @@
-
-
 # Keep an eye on this:
 # https://pcmdi.llnl.gov/CMIP6/ArchiveStatistics/esgf_data_holdings/ScenarioMIP/index.html
 
@@ -7,13 +5,16 @@ import signal
 import time
 from contextlib import contextmanager
 
-class TimeoutException(Exception): pass
+
+class TimeoutException(Exception):
+    pass
 
 
 @contextmanager
 def time_limit(seconds):
     def signal_handler(signum, frame):
         raise TimeoutException("Timed out!")
+
     signal.signal(signal.SIGALRM, signal_handler)
     signal.alarm(seconds)
     try:
@@ -22,10 +23,10 @@ def time_limit(seconds):
         signal.alarm(0)
 
 
-
 import os
 import pandas as pd
 import urllib3
+
 http = urllib3.PoolManager()
 from cmipsearch.models import cmip6_models
 
@@ -33,26 +34,55 @@ from cmipsearch.models import cmip6_models
 # List of possible mirrors
 
 url_list = [
-		"http://esgf.nccs.nasa.gov",
-		"http://esgf-node.llnl.gov",
-		"http://esgf-node.ipsl.upmc.fr",
-		"http://esg-dn1.nsc.liu.se",
-		"http://esgf-data.dkrz.de",
-		"http://esgf-index1.ceda.ac.uk",
-		"http://esg.pik-potsdam.de",
-		"http://esgf.nci.org.au"
-			   ]
+    "http://esgf.nccs.nasa.gov",
+    "http://esgf-node.llnl.gov",
+    "http://esgf-node.ipsl.upmc.fr",
+    "http://esg-dn1.nsc.liu.se",
+    "http://esgf-data.dkrz.de",
+    "http://esgf-index1.ceda.ac.uk",
+    "http://esg.pik-potsdam.de",
+    "http://esgf.nci.org.au",
+]
 # available frequencies
-cmip_freq=['1hr','1hrCM','3hr','3hrPt','6hr','6hrPt','day','dec','fx','mon','monC','monPt','month','subhrPt','yr','yrPt']
+cmip_freq = [
+    "1hr",
+    "1hrCM",
+    "3hr",
+    "3hrPt",
+    "6hr",
+    "6hrPt",
+    "day",
+    "dec",
+    "fx",
+    "mon",
+    "monC",
+    "monPt",
+    "month",
+    "subhrPt",
+    "yr",
+    "yrPt",
+]
 
-def cmip6_search(models = "all",
-        var = None,
-        year_range = None,
-        frequency = "mon",
-        variant = "all",
-        experiment = ["historical", "ssp119", "ssp126", "ssp245", "ssp370", "ssp434", "ssp460", "ssp585", "ssp534-over" ],
-        wait = 60
-        ):
+
+def cmip6_search(
+    models="all",
+    var=None,
+    year_range=None,
+    frequency="mon",
+    variant="all",
+    experiment=[
+        "historical",
+        "ssp119",
+        "ssp126",
+        "ssp245",
+        "ssp370",
+        "ssp434",
+        "ssp460",
+        "ssp585",
+        "ssp534-over",
+    ],
+    wait=60,
+):
 
     """
     Search for CMIP6 data
@@ -84,7 +114,7 @@ def cmip6_search(models = "all",
 
     if year_range is not None:
         run_start = year_range[0]
-        run_end= year_range[1]
+        run_end = year_range[1]
 
     if models != "all":
         if type(models) is str:
@@ -124,11 +154,11 @@ def cmip6_search(models = "all",
             for vv in variant:
                 new_url = f"{new_url}&variant_label={vv}"
 
-        new_url =f"{new_url}&limit=10000"
+        new_url = f"{new_url}&limit=10000"
 
         try:
             with time_limit(wait):
-                response = http.request("GET", new_url )
+                response = http.request("GET", new_url)
 
             lines = response.data.decode("utf-8").split("\n")
             tracker += 1
@@ -142,24 +172,26 @@ def cmip6_search(models = "all",
 
                 if change_level == 2:
                     orig_line = line
-                    years_avail = line[orig_line.index(".nc") -13:orig_line.index(".nc")]
+                    years_avail = line[
+                        orig_line.index(".nc") - 13 : orig_line.index(".nc")
+                    ]
                     year_start = int(years_avail[:4])
                     year_end = int(years_avail[7:11])
                     # maybe remove this if statement later....
                     if var in orig_line:
                         if year_range is not None:
-                            if (year_start <= run_end and year_end >= run_start) or (year_end >= run_start and year_end <= run_end):
+                            if (year_start <= run_end and year_end >= run_start) or (
+                                year_end >= run_start and year_end <= run_end
+                            ):
                                 files.append(line.split(" ")[0])
                                 urls.append(line.split(" ")[1])
                                 check_sums.append(line.split(" ")[3])
                         else:
                             files.append(line.split(" ")[0].replace("'", ""))
                             urls.append(line.split(" ")[1].replace("'", ""))
-                            urls.append(line.split(" ")[1].replace("'", ""))
                             check_sums.append(line.split(" ")[3])
 
-
-    	    # Now pull out the years
+                # Now pull out the years
 
                 if change_level == 1:
                     if "download_files" in line:
@@ -175,14 +207,25 @@ def cmip6_search(models = "all",
     variants = [x.split("_")[4] for x in files]
     experiments = [x.split("_")[3] for x in files]
     variables = [x.split("_")[0] for x in files]
-    starts = [int(x.split("_")[-1][:4]) for x in files ]
-    ends = [int(x.split("_")[-1].split("-")[1][:4]) for x in files ]
-    df = pd.DataFrame({"variable":variables,"experiment":experiments, "model":models, "file":files, "url":urls, "node":nodes, "variant":variants, "start":starts, "end":ends, "checksum":check_sums}).drop_duplicates().reset_index(drop = True)
+    starts = [int(x.split("_")[-1][:4]) for x in files]
+    ends = [int(x.split("_")[-1].split("-")[1][:4]) for x in files]
+    df = (
+        pd.DataFrame(
+            {
+                "variable": variables,
+                "experiment": experiments,
+                "model": models,
+                "file": files,
+                "url": urls,
+                "node": nodes,
+                "variant": variants,
+                "start": starts,
+                "end": ends,
+                "checksum": check_sums,
+            }
+        )
+        .drop_duplicates()
+        .reset_index(drop=True)
+    )
 
     return df
-
-
-
-
-
-
